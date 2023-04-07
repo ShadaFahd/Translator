a0c04455b67d93b8729ec8636db6a87fcd38e222
Shada Alharbi <shada.fahd.h@gmail.com>
2023-04-07 04:27:05 +0300

    first

---
+++ app/src/main/java/com/example/translatorapp/onFailureListener.java
@@ -0,0 +1,11 @@
+package com.example.translatorapp;
+
+
+import androidx.annotation.NonNull;
+
+public class onFailureListener implements com.google.android.gms.tasks.OnFailureListener {
+    @Override
+    public void onFailure(@NonNull Exception e) {
+
+    }
+}
